70ca694cc3ffbc48a6d656ced6ae82a429678b07
Raj nagar <raj@crackerdigital.com>
2020-03-03 18:21:13 +0530

    Vue Nice Table

---
+++ examples/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <VueNiceTable :columns="columns" :rows="rows" />
+  </div>
+</template>
+
+<script>
+import VueNiceTable from "./components/VueNiceTable";
+
+export default {
+  name: "App",
+  components: {
+    VueNiceTable
+  },
+  data() {
+    return {
+      rows: [],
+      columns: [
+        {
+          label: "filed_name",
+          field: "field",
+          tdClass: "text-left",
+          thClass: "text-left"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import Vue from 'vue'
+Vue.component('VueNiceTable', require('./components/VueNiceTable.vue'));
